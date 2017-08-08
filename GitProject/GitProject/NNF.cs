8321c7443b9d27a7a5459e4ef68a0229e3ce08a0
NeandreFourie <25927949@student.g.nwu.ac.za>
2017-08-09 01:53:43 +0200

    updated
    updated commit

---
+++ GitProject/GitProject/NNF.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GitProject
+{
+    public partial class NNF : Form
+    {
+        GenNum GTEngine = new GenNum();
+        stat statistical = new stat();
+        Test tn = new Test();
+        int[] RandomNR;
+
+        public NNF()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAns_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(txtBoxAns.Text);
+                richTextBox1.AppendText("\n");
+                RandomNR = new int[n];
+                for (int i = 0; i <= n - 1; i++)
+                {
+                    RandomNR[i] = GTEngine.Generate();
+                    richTextBox1.AppendText(RandomNR[i] + ", ");
+                }
+                richTextBox1.AppendText("\n");
+                if (tn.Testn(n) == true)
+                {
+                    MessageBox.Show(Convert.ToString("Success: The Mean of the random numbers generated is :" + statistical.calcMean(RandomNR)));
+                }
+                else
+                {
+                    MessageBox.Show("Error: Number not between 5 or 20");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: Please enter a valid number" + ex);
+            }
+           
+        }
+
+        
+        
+    }    
+}
---
+++ GitProject/GitProject/stat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GitProject
+{
+    class stat
+    {
+        public double calcMean(int[] arr)
+        {
+            double ave;
+
+            ave = arr.Average();
+            return  ave;
+        }
+    }
+}
