66922763592d0ae662db509fb12fd4cc06df3038
NeandreFourie <25927949@student.g.nwu.ac.za>
2017-08-09 01:52:49 +0200

    My Deel
    BIO, STAT Method + class

---
+++ GitProject/GenNum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProject
+{
+    class GenNum
+    {
+        Random r = new Random();
+
+        public int Generate()
+        {
+            return r.Next(1, 10);
+        }
+    }
+}
