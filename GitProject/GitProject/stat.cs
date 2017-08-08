04ea1f6d02c43f76b0ab00bcf0b5fd98cacf6702
Chris Bezuidenhout <25820443@student.g.nwu.ac.za>
2017-08-08 17:10:14 -0300

    Random Numbers

---
+++ GitProject/GitProject/stat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProject
+{
+    class stat
+    {
+        private int n;
+        private int[] randNum;
+
+        public stat(int num)
+        {
+            n = num;
+            randNum = new int[num];
+        }
+
+        private void generateNumbers()
+        {
+            Random randomObj = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                randNum[i] = randomObj.Next(100);
+            }
+        }
+    }
+}
